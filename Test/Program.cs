e39214020544795a1eec0fc93c57aaca583d5a77
himanshunete <himanshuneteh@gmail.com>
2021-02-06 11:58:33 +0530

    [himanshunete]Add Choose Letter

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,6 +4,8 @@ namespace Test
 {
     class TicTactoe
     {
+        public const int LETTERX = 0;
+        public const int LETTERO = 1;
         public static string PLAYER_1;
         public static char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
         public static void Display()
@@ -12,6 +14,36 @@ namespace Test
             Console.WriteLine(" Empty Board: " + board);
         }
 
+        public static void ChooseLetter()
+        {
+
+            Random rand = new Random();
+            int letterComputer = rand.Next(0, 2);
+            Console.WriteLine(" Choose a Letter");
+            int letterPlayer = Convert.ToInt32(Console.ReadLine());
+
+            switch (letterPlayer)
+            {
+                case LETTERX:
+                    Console.WriteLine(" Player choose X Option ");
+                    break;
+                case LETTERO:
+                    Console.WriteLine("  Player choose O Option  ");
+                    break;
+            }
+
+            switch (letterComputer)
+            {
+                case LETTERX:
+                    Console.WriteLine(" Computer choose X Option ");
+                    break;
+                case LETTERO:
+                    Console.WriteLine(" Computer choose O Option  ");
+                    break;
+            }
+        }
+
+
     }
     class Program
     {
@@ -19,6 +51,7 @@ namespace Test
         {
             Console.WriteLine(" Welcome to Tic Tac Toe ");
             TicTactoe.Display();
+            TicTactoe.ChooseLetter();
 
         }
     }
