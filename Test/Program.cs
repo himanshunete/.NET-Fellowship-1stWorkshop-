56e73dc47d906e3d775f24f7d5b875106e100d9d
himanshunete <himanshuneteh@gmail.com>
2021-02-06 11:32:49 +0530

    [himanshunete]Ignore FilesAreIgnored

---
+++ Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test
+{
+    class TicTactoe
+    {
+        public static string PLAYER_1;
+        public static char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        public static void Display()
+        {
+            Console.WriteLine(board);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Tic Tac Toe ");
+            TicTactoe.Display();
+
+        }
+    }
+}
